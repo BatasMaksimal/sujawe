eb637ff97223a0a8427214c5e9d18f101e4900bc
Sultan Javier <sultan.javier@gmail.com>
2023-02-01 20:35:31 -0800

    feat: Homepage

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,11 +7,11 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.rtl.min.css" integrity="sha384-WJUUqfoMmnfkBLne5uxXj+na/c7sesSJ32gI7GfCk4zO4GthUKhSEGyvQ839BC51" crossorigin="anonymous">
-
+    
     <title>Jevi Blog | {{ $title}}</title>
   </head>
   <body>
-  include('partials.navbar')
+  
 <div class="container mt-4">
     @yield('container')
 </div>
