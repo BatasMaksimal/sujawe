3dc0fcd458d5daf814f42a61aa8b023a3feca7d4
Rahardiann <yt005881@gmail.com>
2023-02-04 18:28:18 +0700

    feat: update home user

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,20 +1,22 @@
-<nav class="container navbar navbar-expand-lg navbar-dark bg-danger">
-  <a class="navbar-brand" href="#">Jevi Blog</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/about">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/blog">Blog</a>
-      </li>
+<div class="navbar">
+      <nav class="container navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="#">Jevi Blog</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/blog">Blog</a>
+            </li>
 
-    </ul>
-  </div>
-</nav>
+          </ul>
+        </div>
+      </nav>
+</div>
