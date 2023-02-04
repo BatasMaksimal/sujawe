cdf0eb34c03a88ddc3b2ba5a1d6f8e0063cdd84a
Rahardiann <yt005881@gmail.com>
2023-02-04 18:30:23 +0700

    feat: deleted layout and partials

--- resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-@section('container')
-@include('partials.navbar')
-<h3><?= $name?></h3>
-    <p>sultan.javier@gmail.com</p>
-    <img src="img/SLANK.jpeg" alt="sultan" width="200">
-    @endsection
-    
--- resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main')
-@section('container')
-@include('partials.navbar')
-    <h1>Halaman tiga</h1>
-    @endsection
