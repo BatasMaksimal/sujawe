c739b19291e54f627a4db4be015227d1835699d5
Rahardiann <yt005881@gmail.com>
2023-02-10 13:53:00 +0700

    feat: update register view

--- resources/views/admin/tambah_user/tambah_user.blade.php
+++ resources/views/admin/tambah_user/tambah_user.blade.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link rel="stylesheet" href="{{asset('css/stylesheet_login.css')}}">
+    <link rel="stylesheet" href="{{asset('css/user/stylesheet_login.css')}}">
     <link rel="stylesheet" href="{{asset('css/eye.css')}}">
 
 
@@ -61,8 +61,7 @@
                     <?php
                         $arr_role=array('A'=>'Admin','U'=>'User');
                     ?>
-                    <select type="text" name="ROLE" class="form-control my-3 p-3" class="form-control form" placeholder="ROLE" required>
-                        <option></option>
+                    <select type="text" name="ROLE" class="form-control my-2 p-2" class="form-control form" placeholder="ROLE" required>
                             <?php foreach ($arr_role as $key_role => $val_role):?>
                             <option value="<?=$key_role?>"><?=$val_role?></option>
                             <?php endforeach ?>
