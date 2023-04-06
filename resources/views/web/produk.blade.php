7a130f8ac1b71188734f535bec9ffa468867c8ce
Rahardiann <yt005881@gmail.com>
2023-04-06 12:33:51 +0700

    update feat:produk

--- resources/views/web/produk.blade.php
+++ resources/views/web/produk.blade.php
@@ -47,24 +47,7 @@
                             </div>                            
                         </div>
 
-                        <div class="col md-4">
-                            <div class="card-bodai">
-                                <img src="{{asset('img/logo.jpg')}}" class="card-img-top" width="1px"
-                                    height="253px">
-                                <title>Placeholder</title>
-                                <rect width="500%" height="500%" fill="#55595c" /></img>
-                                <div class="card-tampilan">
-                                    <p class="card-text judul-produk"><b>sss</b></p>
-                                    <p class="card-text harga-produk">sss</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <a href=""><button type="submit" class="btn1">Beli</button></a>
-                                        </div>
-                                        <small class="text-muted"></small>
-                                    </div>
-                                </div>
-                            </div>                            
-                        </div>
+                       
                 </div> 
     </div>
 
